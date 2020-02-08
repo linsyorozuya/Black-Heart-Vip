3acc30f9801df9a711c763b8f5e428b2ee081900
linsyorozuya <404875039@QQ.COM>
2020-02-08 22:26:16 +0800

    Update

--- jsDeliverImg.sh
+++ jsDeliverImg.sh
@@ -5,3 +5,5 @@ replaceText="<img src=\"media"
 jsDiliverPre="<img src=\"https://cdn.jsdelivr.net/gh/linsyorozuya/Black-Heart-Vip@master/media"
 
 sed -i "" "s%${replaceText}%${jsDiliverPre}%g" "$readmeFilePath"
+
+echo "replace end."
